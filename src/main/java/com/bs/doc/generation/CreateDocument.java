8f230262ca73fb38f72c20976d13095f0d754e0f
AdamShen <adamshen1997@gmail.com>
2018-10-08 21:34:50 -0700

    update

--- src/main/java/com/bs/doc/generation/CreateDocument.java
+++ src/main/java/com/bs/doc/generation/CreateDocument.java
@@ -26,15 +26,6 @@ public class CreateDocument {
             XWPFDocument document = new XWPFDocument();
             FileOutputStream out = new FileOutputStream(new File("iPlat4C-Weekly.docx"));
 
-            // Add the logo
-            XWPFParagraph image = document.createParagraph();
-            image.setAlignment(ParagraphAlignment.LEFT);
-            XWPFRun imageRun = image.createRun();
-            imageRun.setTextPosition(20);
-            File picture = new File("./assets/bsLogo.png");
-            FileInputStream in = new FileInputStream(picture);
-            imageRun.addPicture(in, Document.PICTURE_TYPE_PNG, "bsLogo.png", 100, 100);
-
             // title setting
             XWPFParagraph title = document.createParagraph();
             title.setAlignment(ParagraphAlignment.CENTER);
@@ -111,86 +102,4 @@ public class CreateDocument {
             System.out.println("unknown error happens when generating the file");
         }
     }
-
-
-//  public static void main(String[] args)throws Exception  {
-//
-//
-//
-//
-//    //Blank Document
-//    XWPFDocument document = new XWPFDocument();
-//
-//    //Write the Document in file system
-//    FileOutputStream out = new FileOutputStream( new File("createdocument.docx"));
-//    XWPFParagraph paragraph = document.createParagraph();
-//    XWPFRun run = paragraph.createRun();
-//
-//
-//    File picture = new File("./assets/bsLogo.png");
-//    System.out.println("directory is: " + (picture.exists()));
-//    FileInputStream in = new FileInputStream(picture);
-//    run.addPicture(in, Document.PICTURE_TYPE_PNG, "bsLogo", 100, 100);
-//
-//    run.setText("At tutorialspoint.com, we strive hard to " +
-//            "provide quality tutorials for self-learning " +
-//            "purpose in the domains of Academics, Information " +
-//            "Technology, Management and Computer Programming Languages.");
-//
-//    //Set bottom border to paragraph
-//    paragraph.setBorderBottom(Borders.BASIC_BLACK_DASHES);
-//
-//    //Set left border to paragraph
-//    paragraph.setBorderLeft(Borders.BASIC_BLACK_DASHES);
-//
-//    //Set right border to paragraph
-//    paragraph.setBorderRight(Borders.BASIC_BLACK_DASHES);
-//
-//    //Set top border to paragraph
-//    paragraph.setBorderTop(Borders.BASIC_BLACK_DASHES);
-//
-//    //create table
-//    XWPFTable table = document.createTable();
-//
-//    //create first row
-//    XWPFTableRow tableRowOne = table.getRow(0);
-//    tableRowOne.getCell(0).setText("col one, row one");
-//    tableRowOne.addNewTableCell().setText("col two, row one");
-//    tableRowOne.addNewTableCell().setText("col three, row one");
-//
-//    //create second row
-//    XWPFTableRow tableRowTwo = table.createRow();
-//    tableRowTwo.getCell(0).setText("col one, row two");
-//    tableRowTwo.getCell(1).setText("col two, row two");
-//    tableRowTwo.getCell(2).setText("col three, row two");
-//
-//    //create third row
-//    XWPFTableRow tableRowThree = table.createRow();
-//    tableRowThree.getCell(0).setText("col one, row three");
-//    tableRowThree.getCell(1).setText("col two, row three");
-//    tableRowThree.getCell(2).setText("col three, row three");
-//
-//    //Set Bold an Italic
-//    XWPFRun paragraphOneRunOne = paragraph.createRun();
-//    paragraphOneRunOne.addBreak();
-//    paragraphOneRunOne.setBold(true);
-//    paragraphOneRunOne.setItalic(true);
-//    paragraphOneRunOne.setText("Font Style");
-//    paragraphOneRunOne.addBreak();
-//
-//    //Set text Position
-//    XWPFRun paragraphOneRunTwo = paragraph.createRun();
-//    paragraphOneRunTwo.setText("Font Style two");
-//    paragraphOneRunTwo.setTextPosition(100);
-//
-//    //Set Strike through and Font Size and Subscript
-//    XWPFRun paragraphOneRunThree = paragraph.createRun();
-//    paragraphOneRunThree.setStrike(true);
-//    paragraphOneRunThree.setFontSize(20);
-//    paragraphOneRunThree.setSubscript(VerticalAlign.SUBSCRIPT);
-//    paragraphOneRunThree.setText(" Different Font Styles");
-//    document.write(out);
-//    out.close();
-//    System.out.println("createdocument.docx written successully");
-//  }
 }
